a4375b1b0d7c89862ac3b9bbe6dcceb1be2199bd
sandypockets <zac@znacreative.com>
2021-08-26 14:59:37 -0400

    Create basic pages

---
+++ pages/create-new-url.jsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout/Layout";
+
+export default function CreateNewUrl() {
+  return (
+    <Layout>
+      Create a new URL!
+    </Layout>
+  )
+}
---
+++ pages/your-urls.jsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout/Layout";
+
+export default function YourUrls() {
+  return (
+    <Layout>
+      Your URLs!
+    </Layout>
+  )
+}
