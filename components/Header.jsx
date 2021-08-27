95623fa1fa168b0150331546427ffaa7a4f0596e
sandypockets <zac@znacreative.com>
2021-08-27 10:59:13 -0400

    Add Header to HomePage

---
+++ components/Header.jsx
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <div className="bg-white">
+      <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <h2 className="text-base font-semibold text-indigo-600 tracking-wide uppercase">Shortn</h2>
+          <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+            Keep your brand looking professional
+          </p>
+          <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+            Forget about sharing ugly UTM tracking links with your clients. Share easy-on-the-eyes shortn links instead.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
