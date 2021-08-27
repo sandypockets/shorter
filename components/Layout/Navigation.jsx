d9de96e2d4cbf4b6532e9ccc5cfd8bc2b2fd198a
sandypockets <zac@znacreative.com>
2021-08-27 14:42:10 -0400

    Remove unused icon

--- components/Layout/Navigation.jsx
+++ components/Layout/Navigation.jsx
@@ -56,7 +56,6 @@ export default function Navigation({ currentUrl }) {
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
                   <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
 
                 {/* Profile dropdown */}
