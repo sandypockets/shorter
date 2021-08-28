2107c6a99cc489cb6f27c559767c115c81882e76
sandypockets <zac@znacreative.com>
2021-08-28 12:11:45 -0400

    Adjust margins

--- pages/account.jsx
+++ pages/account.jsx
@@ -1,10 +1,13 @@
 import Layout from "../components/Layout/Layout";
 import Account from "../components/Account/Account";
+import Container from "../components/Layout/Container";
 
 export default function AccountPage() {
   return (
     <Layout>
-      <Account />
+      <div className="mt-10">
+        <Account />
+      </div>
     </Layout>
   )
 }
