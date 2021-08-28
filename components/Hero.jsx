22208236602e5e0d0e2848bd8094086e419a2eb5
sandypockets <zac@znacreative.com>
2021-08-28 15:47:51 -0400

    Add links to CTA

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -14,7 +14,7 @@ export default function Hero() {
           <div className="mt-10 sm:flex sm:justify-center lg:justify-start">
             <div className="rounded-md shadow">
               <a
-                href="#"
+                href="/create-new-url"
                 className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700 md:py-4 md:text-lg md:px-10"
               >
                 Create a short URL
@@ -22,7 +22,7 @@ export default function Hero() {
             </div>
             <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
               <a
-                href="#"
+                href="/features"
                 className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-green-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
               >
                 Learn more
