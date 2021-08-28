8bde7027f8a9f530031c28367c5a729fa3879fc8
sandypockets <zac@znacreative.com>
2021-08-28 10:09:59 -0400

    Add paragraph below heading

--- components/FeatureList.jsx
+++ components/FeatureList.jsx
@@ -41,7 +41,9 @@ export default function FeatureList() {
       <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
         <div className="max-w-3xl mx-auto text-center">
           <h2 className="text-3xl font-extrabold text-gray-900">The only link shortener you'll ever need</h2>
-
+          <p className="mt-4 text-lg text-gray-500">
+            Features and plans, tailored to content creators and startups.
+          </p>
         </div>
         <dl className="mt-12 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 lg:grid-cols-4 lg:gap-x-8">
           {features.map((feature) => (
