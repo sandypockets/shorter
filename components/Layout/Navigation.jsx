26fcbe60c72e9481d66aba22213929d476687031
sandypockets <zac@znacreative.com>
2021-08-28 11:27:42 -0400

    Fix z-index of dropdown menu

--- components/Layout/Navigation.jsx
+++ components/Layout/Navigation.jsx
@@ -84,7 +84,7 @@ export default function Navigation({ currentUrl }) {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="z-10 origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
                           <Link href="/account">
