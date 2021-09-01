ca5dc284dc68eb1d7e4cf4ed6fbb8bd75994a97d
sandypockets <zac@znacreative.com>
2021-09-01 00:11:45 -0400

    Add empty state to your-urls page

--- pages/your-urls.jsx
+++ pages/your-urls.jsx
@@ -9,6 +9,7 @@ import SlideOver from "../components/Utils/SlideOver";
 import Modal from "../components/Utils/Modal";
 import Container from "../components/Layout/Container";
 import SignIn from "../components/Account/Auth";
+import EmptyUrls from "../components/Utils/EmptyUrls";
 
 export default function YourUrls() {
   const [urlsList, setUrlsList] = useState([])
@@ -91,8 +92,16 @@ export default function YourUrls() {
                 <h1 className="text-4xl mt-24 mb-10 tracking-tight font-extrabold flex justify-center">
                   Your short URLs
                 </h1>
-                <Table setShowModal={setShowModal} urlsList={urlsList} urlId={urlId} setUrlId={setUrlId} open={open} setOpen={setOpen} setShortUrl={setShortUrl} setCurrentLongUrl={setCurrentLongUrl} />
-                <SlideOver setEditedUrl={setEditedUrl} urlId={urlId} open={open} setOpen={setOpen} shortUrl={shortUrl} currentLongUrl={currentLongUrl} setCurrentLongUrl={setCurrentLongUrl} />
+                {urlsList.length > 1 ? (
+                  <>
+                    <Table setShowModal={setShowModal} urlsList={urlsList} urlId={urlId} setUrlId={setUrlId} open={open} setOpen={setOpen} setShortUrl={setShortUrl} setCurrentLongUrl={setCurrentLongUrl} />
+                    <SlideOver setEditedUrl={setEditedUrl} urlId={urlId} open={open} setOpen={setOpen} shortUrl={shortUrl} currentLongUrl={currentLongUrl} setCurrentLongUrl={setCurrentLongUrl} />
+                  </>
+                ) : (
+                  <>
+                  <EmptyUrls />
+                  </>
+                )}
                 {showModal && (
                   <Modal deleteLoading={deleteLoading} deleteUrl={deleteUrl} showModal={showModal} setShowModal={setShowModal} urlId={urlId} setUrlId={setUrlId} />
                 )}
