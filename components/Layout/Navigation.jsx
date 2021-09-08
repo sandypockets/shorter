5493ca949cd214517cfa70cf8ae56113aa6e2462
sandypockets <zac@znacreative.com>
2021-09-08 09:49:21 -0400

    Add 'Home' to mobile nav

--- components/Layout/Navigation.jsx
+++ components/Layout/Navigation.jsx
@@ -196,6 +196,15 @@ export default function Navigation({ currentUrl, session, url, userData, setUser
           </div>
 
           <Disclosure.Panel className="sm:hidden">
+            <div className="pt-2 pb-4 space-y-1">
+              <Link href={'/'}>
+                <a
+                  className={currentUrl === '/' ? "bg-green-50 border-green-500 text-green-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium" : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"}
+                >
+                  Home
+                </a>
+              </Link>
+            </div>
             {navItems.map((item, index) => (
               <div key={index} className="pt-2 pb-4 space-y-1">
                 <Link href={item.href}>
